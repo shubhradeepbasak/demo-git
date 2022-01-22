de928cf4790679483ce12f23616a1db490fd2a34
hubhra Deep Basak <shubhradeepbasak@gmail.com>
2022-01-23 00:36:17 +0530

    first commit

---
+++ src/test/DemoClass.java
@@ -0,0 +1,12 @@
+package test;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+		System.out.println("World" );
+
+	}
+
+}
