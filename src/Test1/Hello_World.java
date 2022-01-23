7f13a1b40386fc8a8b6bd87a866e4f298b469f4e
shubhradeepbasak <shubhradeepbasak@gmail.com>
2022-01-23 22:50:20 +0530

    first commit

---
+++ src/Test1/Hello_World.java
@@ -0,0 +1,14 @@
+package Test1;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+		System.out.println("World");
+
+
+
+	}
+
+}
